b5e5b3e15c52995e409a8cd6c771aef01558c242
Matias Rojas <ar.rojas.matias@gmail.com>
2019-07-27 13:14:49 -0300

    Add first version of project (#1)

---
+++ src/test/java/com/intercorpretail/www/mortality/prediction/MortalityPredictionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.intercorpretail.www.mortality.prediction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MortalityPredictionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
