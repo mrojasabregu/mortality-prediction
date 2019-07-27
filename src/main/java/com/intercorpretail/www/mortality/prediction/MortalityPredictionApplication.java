135ed80da33a01e41155581296dc0e07c6de2a3b
Matias Rojas <ar.rojas.matias@gmail.com>
2019-07-27 13:13:48 -0300

    Add first version of project

---
+++ src/main/java/com/intercorpretail/www/mortality/prediction/MortalityPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.intercorpretail.www.mortality.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortalityPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortalityPredictionApplication.class, args);
+	}
+
+}
